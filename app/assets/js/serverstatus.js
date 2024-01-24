bad9e8ac91d5576589b2e94112bf6e1b52abb168
Thermidoren <zhyntos14@gmail.com>
2024-01-24 06:09:11 +0100

    Modification 1 background

--- app/assets/js/serverstatus.js
+++ app/assets/js/serverstatus.js
@@ -3,8 +3,8 @@ const net = require('net')
 /**
  * Retrieves the status of a minecraft server.
  * 
- * @param {string} address The server address.
- * @param {number} port Optional. The port of the server. Defaults to 25565.
+ * @param {51.38.60.133} address The server address.
+ * @param {25565} port Optional. The port of the server. Defaults to 25565.
  * @returns {Promise.<Object>} A promise which resolves to an object containing
  * status information.
  */
